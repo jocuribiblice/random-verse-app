49d220a1b620a2dda3ee06b01dc783e1cded283f
GhitaB <ghitabizau27@gmail.com>
2022-10-04 22:40:39 +0300

    Use button.

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.
+      </Text>
+      <Text>
+        (Lorem 2:7)
+      </Text>
+      <Button title='Next' />
     </View>
   );
 }
